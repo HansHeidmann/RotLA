4a0e7046b1083f2468f9a037e5c95917cdecd90b
Hans Heidmann <9998078+HansHeidmann@users.noreply.github.com>
2022-09-20 14:52:29 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+
         BoardRenderer renderer = new BoardRenderer();
-        renderer.displayGameState();
+
+        while (!renderer.gameOver) {
+            // main loop
+            renderer.displayGameState();
+            renderer.takeTurn();
+            // sleep
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
     }
 }
