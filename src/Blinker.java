0795288c5271d1ca2b9208e35318a199989f5b92
Hans Heidmann <9998078+HansHeidmann@users.noreply.github.com>
2022-09-20 19:23:37 -0700

    Update Blinker.java

--- src/Blinker.java
+++ src/Blinker.java
@@ -1,2 +1,15 @@
 public class Blinker extends Creature {
+
+    @Override
+    public void move() {
+        gotoRoom(1 + (int) (Math.random() * 4), (int) (Math.random() * 3), (int) (Math.random() * 3));
+    }
+
+    private void gotoRoom(int y, int x, int z) {
+        Room targetRoom = currentRoom.renderer.getRoomByID(y + "-" + x + "-" + z);
+        currentRoom.removeCreature(this);
+        targetRoom.addCreature(this);
+        currentRoom = targetRoom;
+    }
+
 }
