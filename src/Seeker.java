cefc71feccb1c98caad5dbec7fa1581bab2f950b
Hans Heidmann <9998078+HansHeidmann@users.noreply.github.com>
2022-09-20 19:27:43 -0700

    Update Seeker.java

--- src/Seeker.java
+++ src/Seeker.java
@@ -1,2 +1,15 @@
 public class Seeker extends Creature {
+
+    public void move() {
+        if(currentRoom.adventurers.size() == 0) {
+            for(Room room: currentRoom.adjacentRooms) {
+                if(room.adventurers.size() > 0) {
+                    currentRoom.removeCreature(this);
+                    room.addCreature(this);
+                    currentRoom = room;
+                }
+            }
+        }
+    }
+
 }
